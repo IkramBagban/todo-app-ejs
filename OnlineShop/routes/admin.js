540d3feaf3bc8bf499e5b9b20a35cc0e12337fe3
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-11 15:12:55 +0530

    controller error

---
+++ OnlineShop/routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require("../controllers/products")
+
+const router = express.Router();
+
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProducts);
+
+// /admin/add-product => POST
+router.post('/add-product',productsController.postAddProducts);
+
+module.exports = router
