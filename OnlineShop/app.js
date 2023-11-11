05cebff8f3b2870ddd8c207666977cf3936f2404
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-11 17:40:38 +0530

    Inhanced the app

--- OnlineShop/app.js
+++ OnlineShop/app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
-
-const errorController = require('./controllers/error')
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
--- OnlineShop/models/product.js
+++ OnlineShop/models/product.js
@@ -1,32 +1,35 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json"
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
-    if (err) return cb([]);
-
-    cb(JSON.parse(fileContent));
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      getProductsFromFile((products) => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
