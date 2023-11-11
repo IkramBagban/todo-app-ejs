66a7797c72f51dcf33c2338c745ba48e53368858
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-11 16:33:50 +0530

    Stroging data in files via Model. Have some length error

--- OnlineShop/models/product.js
+++ OnlineShop/models/product.js
@@ -1,17 +1,37 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const product = [];
 
 module.exports = class Product {
-    constructor(t){
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save(){
-        products.push(this)
-    }
+  save() {
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
 
-    static fetchAll(){
-        return products
-    }
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-
-}
+  static fetchAll() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
+  }
+};
