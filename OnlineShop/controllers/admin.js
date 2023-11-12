9cd4033e3035abc24c870fb0020fcd8e372ce320
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-12 23:48:59 +0530

    Displayinh The Cart Items on the cart page

--- OnlineShop/controllers/admin.js
+++ OnlineShop/controllers/admin.js
@@ -1,115 +1,71 @@
-const { log } = require("console");
-const Product = require("../models/product");
-const fs = require("fs");
-const path = require("path");
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/edit-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    editing: false,
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log("Post add product");
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
   product.save();
-  res.redirect("/");
+  res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
-  console.log("getEditProduct");
   const editMode = req.query.edit;
-  // console.log("req" , editMode)
-  console.log("editmode", !!editMode);
   if (!editMode) {
-    return res.redirect("/");
+    return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
+  Product.findById(prodId, product => {
     if (!product) {
-      return res.redirect("/");
+      return res.redirect('/');
     }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
       editing: editMode,
-      product: product,
+      product: product
     });
   });
 };
 
 exports.postEditProduct = (req, res, next) => {
-  console.log("EDIT");
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
-  const updateImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
   const updatedPrice = req.body.price;
-
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
   const updatedProduct = new Product(
     prodId,
     updatedTitle,
-    updateImageUrl,
+    updatedImageUrl,
     updatedDesc,
     updatedPrice
   );
-  console.log("udpate product", updatedProduct);
   updatedProduct.save();
-
-  // redirect to admin page.
-  res.redirect("/admin/products");
-};
-
-exports.deleteProduct = (req, res, next) => {
-  console.log("deleteProduct");
-  const prodId = req.body.productId;
-  // console.log("deleting product",prodId)
-
-  const p = path.join(
-    path.dirname(process.mainModule.filename),
-    "data",
-    "products.json"
-  );
-
- 
-
-
-  fs.readFile(p, (err, products) => {
-    console.log("enterd in reddfile");
-    if (err) {
-      console.log("got an error while reading data");
-      return;
-    }
-
-    products = JSON.parse(products);
-    const deleteItemIndex = products.findIndex((prods) => prodId === prods.id);
-    const updatedProductsAfterDeleting = products.filter(
-      (prods) => prods.id !== prodId
-    );
-    fs.writeFile(p, JSON.stringify(updatedProductsAfterDeleting), (err) => {
-      if (err) console.log("there is something error while deleting.");
-    });
-
-    console.log("deleteItem  ", products[deleteItemIndex]);
-  });
-
-  // redirect to admin page.
-  res.redirect("/admin/products");
-
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
       prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
--- OnlineShop/models/product.js
+++ OnlineShop/models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -17,8 +19,6 @@ const getProductsFromFile = cb => {
   });
 };
 
-
-
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
     this.id = id;
@@ -29,24 +29,35 @@ module.exports = class Product {
   }
 
   save() {
-
-
     getProductsFromFile(products => {
-      if(this.id){
-        const existingProductIndex = products.findIndex(prods => prods.id === this.id);
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
         const updatedProducts = [...products];
         updatedProducts[existingProductIndex] = this;
         fs.writeFile(p, JSON.stringify(updatedProducts), err => {
           console.log(err);
         });
-      }else{
-        
+      } else {
         this.id = Math.random().toString();
         products.push(this);
         fs.writeFile(p, JSON.stringify(products), err => {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 
