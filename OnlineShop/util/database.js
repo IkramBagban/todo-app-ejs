d10b1ccb4f25b59001890d1feb80126cb2875010
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-14 17:47:27 +0530

    Connect To Database

--- OnlineShop/util/database.js
+++ OnlineShop/util/database.js
@@ -1,14 +1,23 @@
-const mysql = require('mysql2');
-const pass = require('../pass');
+// const mysql = require('mysql2');
+const pass = require("../pass");
 
-const password =  require('../pass.js')
+// const password =  require('../pass.js')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user : 'root',
-    database: 'onlineshop',
-    password : password
-})
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user : 'root',
+//     database: 'onlineshop',
+//     password : password
+// })
 
+// module.exports = pool.promise();
 
-module.exports = pool.promise();
+// sequelize
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("onlineshop", "root", paas, {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
