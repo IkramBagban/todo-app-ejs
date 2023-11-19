9819519fca8d0b1f0393c44103927c679febed39
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-19 14:38:20 +0530

    Updating Products. Using findByPk() method to find product. and save() method to save the updated data

--- OnlineShop/controllers/admin.js
+++ OnlineShop/controllers/admin.js
@@ -32,17 +32,19 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product: product,
-    });
-  });
+  Product.findByPk(prodId)
+    .then((product) => {
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((e) => console.log(e));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -51,26 +53,23 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
+
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    })
+    .then((result) => {
+      console.log("PRODUCT UPDATED");
+      res.redirect("/admin/products");
+    })
+    .catch((e) => console.log(e));
 };
 
 exports.getProducts = (req, res, next) => {
-  // Product.fetchAll((products) => {
-  //   res.render("admin/products", {
-  //     prods: products,
-  //     pageTitle: "Admin Products",
-  //     path: "/admin/products",
-  //   });
-  // });
-
   Product.findAll()
     .then((products) => {
       res.render("admin/products", {
