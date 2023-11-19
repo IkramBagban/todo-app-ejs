2b54ef99f9bb0770614908d38f90ff301511bb39
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-19 14:11:18 +0530

    Find product By Id using Model.findById() method

--- OnlineShop/controllers/shop.js
+++ OnlineShop/controllers/shop.js
@@ -15,18 +15,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findById(prodId, (product) => {});
-
-  Product.findById(prodId)
-    .then(([product]) => {
-      console.log(product[0]);
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
-        pageTitle: product[0].title,
+        product: product,
+        pageTitle: product.title,
         path: "/products",
       });
     })
-    .catch((e) => console.log(e));
+    .catch((e) => console.log("Get An Error While Fetching A Single Product", e));
 };
 
 exports.getIndex = (req, res, next) => {
