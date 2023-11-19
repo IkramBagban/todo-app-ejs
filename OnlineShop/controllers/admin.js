767eabe0a848865c41cbf676838da25afa6ff36a
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-19 14:16:35 +0530

    Fetching Admin Products

--- OnlineShop/controllers/admin.js
+++ OnlineShop/controllers/admin.js
@@ -63,13 +63,23 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  });
+  // Product.fetchAll((products) => {
+  //   res.render("admin/products", {
+  //     prods: products,
+  //     pageTitle: "Admin Products",
+  //     path: "/admin/products",
+  //   });
+  // });
+
+  Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((e) => console.log(e));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
