b62b706a2ea2d37bec9ed7cc04f069b7d58d8aa9
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-19 15:55:06 +0530

    Created And Managing a Dummy User

--- OnlineShop/app.js
+++ OnlineShop/app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 
-const Product = require('./models/product')
-const User = require('./models/user')
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,19 +20,37 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'})
-User.hasMany(Product)
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
 
 sequelize
+  // .sync({force:true})
   .sync()
-  .then(result =>{
-    console.log(result)
+  .then((result) => {
+    return User.findByPk(1);
   })
-  .catch((e) => console.log('e=> ',e));
-
-app.listen(3000);
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Test", email: "test@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
+    app.listen(3000);
+  })
+  .catch((e) => console.log("e=> ", e));
